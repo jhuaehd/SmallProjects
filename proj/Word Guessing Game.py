89dbd1826201278396da248540a2642f820cc9df
Joshua Ehd Ehd <joshuaehd.silvio.37@gmail.com>
2022-06-16 06:29:15 +0000

    created hints

--- proj/Word Guessing Game.py
+++ proj/Word Guessing Game.py
@@ -1,16 +1,38 @@
 import random
 
-words = ['Python', 'Java', 'C++', 'Ruby', 'Nodejs', 'PHP', 'Laravel']
+words = ['Python', 'Java', 'C++', 'Ruby', 'Nodejs', 'PHP', 'Laravel', 'Reactjs', 'HTML', 'SQL', 'ASP', 'Git']
+
 print(words, "\n")
-print("Guess the word from the given list")
+print("\nGuess the word from the given list")
 
+hints = ['Snake', 
+         'Root crop',
+         'Increment',
+         'Shining',
+         'non-blocking, event-driven servers',
+         'Web Database',
+         'an open-source PHP framework',
+         'a JavaScript library',
+         'Markup Language',
+         'Query',
+         'development framework for building web pages',
+         'an Open Source Distributed Version Control System'
+        ]
 
+# list the words using for loop
 for word in words:
   word = random.choice(words)
-  
-print("Word has ", len(word), "characters")
+
+# get the length of a random word
+print("\nWord has ", len(word), "characters")
+# get the index to print the hint for the random word
+hint = words.index(word)
+print("Hint: ", hints[hint])
 
 lives = 5
+print("\nYou have 5 lives to guess, there's also a hint for you to guess faster.\n")
+
+# guessing loop according to number of lives
 while lives > 0:
   userAns = input()
   if userAns == word:
