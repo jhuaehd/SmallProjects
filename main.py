ea782e6544b94ebd0ba65dcae34c4583a4652bd4
Joshua Ehd Ehd <joshuaehd.silvio.37@gmail.com>
2022-06-16 05:58:13 +0000

    update guess number

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import random
 
-yr = int(input("Enter your year of birth: "))
+words = ['Python', 'Java', 'C++', 'Ruby', 'Nodejs', 'PHP', 'Laravel']
+print(words, "\n")
+print("Guess the word from the given list")
+for word in words:
+  word = random.choice(words)
+  
+print("Word has ", len(word), "characters")
 
-for y in range(1, 2023):
-  if y == yr:
-    print(y)
+lives = 5
+while lives > 0:
+  userAns = input()
+  if userAns == word:
+    print("Awesome, You won !")
+    break
+  else:
+    lives = lives - 1
+    print("Wrong Guess! You have ", lives, "remaining lives")
+
+else:
+  print("You lose")
