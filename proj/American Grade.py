378e6e6657cd38d7eabf3940f56955963b153b64
Joshua Ehd Ehd <joshuaehd.silvio.37@gmail.com>
2022-06-16 07:44:32 +0000

    add file

---
+++ proj/American Grade.py
@@ -0,0 +1,20 @@
+grade = float(input())
+if 0 <= grade <= 10:
+    if grade % 0.5 == 0:
+        # Check if Grade A
+        if 10 >= grade >= 8.5:
+            print("Grade A")
+        elif 8 >= grade >= 7.5:
+            print("Grade B")
+        elif 7 >= grade >= 6.5:
+            print("Grade C")
+        elif 5.5 >= grade >= 6:
+            print("Grade D")
+        elif 5 > grade >= 0:
+            print("Grade F")
+        else:
+            print("Grades should be between zero and 10.")
+    else:
+        print("Grades should be rounded to the nearest half point.")
+else:
+    print("Grades should be between zero and 10.")
